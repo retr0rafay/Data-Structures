664d477052fed4d3ea572dbdb3efb70eab2d625c
Rafay Syed <32440307+retr0rafay@users.noreply.github.com>
2020-10-13 23:05:08 -0500

    Create MergeSortedArrays.py

---
+++ MergeSortedArrays.py
@@ -0,0 +1,14 @@
+def mergeSortedArrays(array1, array2):
+  bigArray = array1 + array2
+
+  for i in range(len(bigArray)):
+    for j in range(i + 1, len(bigArray)):
+      if bigArray[i] >= bigArray[j]:
+        num = bigArray[i]
+        bigArray[i] = bigArray[j]
+        bigArray[j] = num
+        
+      
+  return bigArray
+
+print(mergeSortedArrays([0,3,4,5,31], [4,6,32]))
